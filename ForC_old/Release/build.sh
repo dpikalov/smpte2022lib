256c67b8bc6982f48d78dfd442b8533aaab0f4bd
dpikalov <pikalov.denis@gmail.com>
2023-07-24 13:47:45 +0300

    WIP

--- ForC_old/Release/build.sh
+++ ForC_old/Release/build.sh
@@ -1,4 +1,4 @@
-cd ./CodeBlocks
+cd ../CodeBlocks
 
 gcc -fexpensive-optimizations -O2 -Wmain  -c ../Code/algo_structs/sBufferFec.c -o ../Release/Code/algo_structs/sBufferFec.o
 gcc -fexpensive-optimizations -O2 -Wmain  -c ../Code/algo_structs/sBufferMedia.c -o ../Release/Code/algo_structs/sBufferMedia.o
@@ -16,10 +16,10 @@ gcc -fexpensive-optimizations -O2 -Wmain  -c ../Code/data_structs/sLinkedList.c 
 gcc -fexpensive-optimizations -O2 -Wmain  -c ../Code/data_structs/sRbTree.c -o ../Release/Code/data_structs/sRbTree.o
 gcc -fexpensive-optimizations -O2 -Wmain  -c ../Code/utilities/sTewfiq.c -o ../Release/Code/utilities/sTewfiq.o
 gcc -Wall -O2  -c ../Code/demonstrateurs/FecGenerator.c -o ../Code/demonstrateurs/FecGenerator.o
-gcc  -o ../Release/FecGenerator ../Code/demonstrateurs/FecGenerator.o  -s  ../Release/libSmpte-2022-.a
 
 rm -f ../Release/libSmpte-2022-.a
 ar -r -s ../Release/libSmpte-2022-.a ../Release/Code/algo_structs/sBufferFec.o ../Release/Code/algo_structs/sBufferMedia.o ../Release/Code/algo_structs/sCrossFec.o ../Release/Code/algo_structs/sPaquetFec.o ../Release/Code/algo_structs/sPaquetMedia.o ../Release/Code/algo_structs/sSeqNx.o ../Release/Code/algo_structs/sWaitFec.o ../Release/Code/algorithmes/sBruteSmpte.o ../Release/Code/algorithmes/sDavidSmpte.o ../Release/Code/common/console.o ../Release/Code/common/messages.o ../Release/Code/data_structs/sChampBits.o ../Release/Code/data_structs/sLinkedList.o ../Release/Code/data_structs/sRbTree.o ../Release/Code/utilities/sTewfiq.o
+gcc  -o ../Release/FecGenerator ../Code/demonstrateurs/FecGenerator.o  -s  ../Release/libSmpte-2022-.a
 
 # FecGenerator
 gcc -Wall -O2  -c ../Code/demonstrateurs/FecGenerator.c -o ../Code/demonstrateurs/FecGenerator.o
@@ -30,5 +30,7 @@ gcc -Wall -O2 -c ../Code/demonstrateurs/FecDecoder.c -o ../Code/demonstrateurs/F
 gcc -o ../Release/FecDecoder ../Code/demonstrateurs/FecDecoder.o ../Release/Code/algo_structs/sSeqNx.o -s  ../Release/libSmpte-2022-.a
 
 # ErrorsGenerator
-gcc -Wall -O2 -W -Wall  -c ../Code/demonstrateurs/ErrorsGenerator.c -o ../Code/demonstrateurs/ErrorsGenerator.o
-gcc  -o ../Release/ErrorsGenerator ../Code/demonstrateurs/ErrorsGenerator.o  -s  ../Release/libSmpte-2022-.a
+#gcc -Wall -O2 -W -Wall  -c ../Code/demonstrateurs/ErrorsGenerator.c -o ../Code/demonstrateurs/ErrorsGenerator.o
+#gcc  -o ../Release/ErrorsGenerator ../Code/demonstrateurs/ErrorsGenerator.o  -s  ../Release/libSmpte-2022-.a
+
+cd -
